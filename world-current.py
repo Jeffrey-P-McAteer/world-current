3de0c7830ea5627ee20e73fab27e6bd91cb6161f
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-05-16 19:28:36 -0400

    better

--- world-current.py
+++ world-current.py
@@ -21,19 +21,21 @@ config.toml contains configuration like the following:
 ```
 model_name = "Your Model's Name"
 
-# At least one region_* definition must be given; if a file or layer of data is specified, ALL contents
+# At exactly one of the region definitions below must be given; if a file or layer of data is specified, ALL contents
 # of the file/layer will be used to select regions of the world for processing.
 
 # The bounding box is specified as MinX, MinY, MaxX, MaxY in WGS84 coordinates
-region_bbox = [-82.9657830472, 7.2205414901, -77.2425664944, 9.61161001224]
+region = [-82.9657830472, 7.2205414901, -77.2425664944, 9.61161001224]
 
-# If this is used all polygons in the file will be used to select region
-region_geojson = "./path/to/layer.geojson"
+# If this is used all polygons in the file will be used to select region; formats supported are
+# everything that geopandas supports.
+region = "./path/to/layer.geojson"
 
-# region_wkt may also contain inline POLYGON() wkt instead of a file-path to a text file.
+# region may also contain inline POLYGON() wkt instead of a file-path to a text file.
 # In this case whitespace will be removed from the string before parsing.
-region_wkt = "./path/to/wkt.txt"
-
+region = """
+POLYGON(-82.9657830472 7.2205414901, -77.2425664944 9.61161001224, -77.1425664944 9.41161001224, -77.0325664944 9.11161001224, -82.9657830472 7.2205414901)
+"""
 ```
 
 '''.strip())
